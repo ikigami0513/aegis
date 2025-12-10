f273de65aec04ff5f8fc0fc998b2430c9c2cc134
ikigami0513 <ikigami0513@users.noreply.github.com>
2025-12-10 16:28:22 +0000

    deploy: f70fb3778fa5746810e59063f6dda2d9c02a07d3

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9e7bdb3a.js',
+                path_to_root + 'searchindex-d985e5a0.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
