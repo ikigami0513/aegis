538e66e36d142d7e29dfe72ebb143d620d40d725
Ikigami0513 <ikigami0513@gmail.com>
2025-12-11 12:06:59 +0100

    #1

--- src/native/system.rs
+++ src/native/system.rs
@@ -3,6 +3,7 @@ use std::{collections::HashMap, io::{self, Write}};
 
 pub fn register(map: &mut HashMap<String, super::NativeFn>) {
     map.insert("io_clear".to_string(), io_clear);
+    map.insert("io_write".to_string(), io_write);
     map.insert("sys_env".to_string(), sys_env);
     map.insert("sys_fail".to_string(), sys_fail);
 }
@@ -11,6 +12,13 @@ fn io_clear(_: Vec<Value>) -> Result<Value, String> {
     // Petit hack cross-platform pour nettoyer le terminal
     print!("\x1B[2J\x1B[1;1H"); 
     io::stdout().flush().unwrap();
+    Ok(Value::Null)
+}
+
+fn io_write(args: Vec<Value>) -> Result<Value, String> {
+    let s = args[0].as_str()?;
+    print!("{}", s); // Pas de println!
+    std::io::stdout().flush().unwrap();
     Ok(Value::Null)
 }
 
