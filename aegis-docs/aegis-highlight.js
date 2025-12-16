b75ed593829b01d2711cbb6dee7659e34b29713f
Ikigami0513 <ikigami0513@gmail.com>
2025-12-16 13:54:33 +0100

    update highlight for mdbook

--- aegis-docs/aegis-highlight.js
+++ aegis-docs/aegis-highlight.js
@@ -2,24 +2,47 @@
 hljs.registerLanguage('aegis', function(hljs) {
     return {
         name: 'Aegis',
-        aliases: ['aeg'], // Permet d'utiliser ```aeg aussi
+        aliases: ['aeg'],
         keywords: {
-            // Mots-clés de contrôle et structure
-            keyword: 'var func class namespace import return if else while for switch case default try catch throw new print input break continue',
+            // Mots-clés de structure et contrôle
+            keyword: 
+                'var func class interface namespace import return ' +
+                'if else while foreach in switch case default ' +
+                'try catch throw new print input break continue ' +
+                // Nouveaux mots-clés POO (v0.4+)
+                'static final public private protected extends implements init',
+            
             // Valeurs littérales
             literal: 'true false null',
-            // Variables spéciales ou modules natifs courants
-            built_in: 'this System File Http Json Math Random Test Assert Base64 Hash Date Process Path',
-            // Tes types pour le typage graduel
-            type: 'int float string bool list dict func'
+            
+            // Variables spéciales et Modules natifs
+            built_in: 
+                'this super ' + // Ajout de super
+                'System File Http Json Math Random Test Assert Base64 Hash Date Process Path ' +
+                'Socket Sqlite Bytes', // Nouveaux modules v0.4.3
+            
+            // Types
+            type: 'int float string bool list dict func bytes void any'
         },
         contains: [
-            hljs.QUOTE_STRING_MODE,       // "string"
+            // Chaînes de caractères avec interpolation ${...}
+            {
+                className: 'string',
+                begin: '"', end: '"',
+                contains: [
+                    hljs.BACKSLASH_ESCAPE,
+                    {
+                        className: 'subst', // Pour colorer ${var}
+                        begin: '\\$\\{', end: '\\}',
+                        keywords: { keyword: 'this', built_in: 'this' }
+                    }
+                ]
+            },
             hljs.C_LINE_COMMENT_MODE,     // // comment
             hljs.C_BLOCK_COMMENT_MODE,    // /* comment */
-            hljs.NUMBER_MODE,             // 123, 12.5
+            hljs.NUMBER_MODE,             // 123, 12.5, 0xFF
             
-            // Coloration spécifique pour la définition de fonctions (func nom)
+            // Définition de fonctions
             {
                 className: 'function',
                 beginKeywords: 'func', 
@@ -29,19 +52,30 @@ hljs.registerLanguage('aegis', function(hljs) {
                     hljs.TITLE_MODE,
                     {
                         className: 'params',
-                        begin: /\(/, end: /\)/
+                        begin: /\(/, end: /\)/,
+                        // On permet de colorer les types dans les params (ex: data: dict)
+                        keywords: { type: 'int float string bool list dict func bytes void any' }
                     }
                 ]
             },
-            // Coloration spécifique pour les classes et namespaces
+            
+            // Définition de classes, interfaces et namespaces
             {
                 className: 'class',
-                beginKeywords: 'class namespace', 
+                beginKeywords: 'class interface namespace', 
                 end: /\{/, 
                 excludeEnd: true,
-                contains: [hljs.TITLE_MODE]
+                contains: [
+                    hljs.TITLE_MODE,
+                    // Pour gérer "class User extends Model" ou "implements Interface"
+                    {
+                        beginKeywords: 'extends implements',
+                        relevance: 0
+                    }
+                ]
             },
-            // Décorateurs (@logger)
+            
+            // Décorateurs
             {
                 className: 'meta',
                 begin: '@[a-zA-Z_][a-zA-Z0-9_]*'
@@ -50,5 +84,5 @@ hljs.registerLanguage('aegis', function(hljs) {
     };
 });
 
-// Relance la coloration si le script est chargé après le DOM
+// Relance auto au chargement
 hljs.initHighlightingOnLoad();
